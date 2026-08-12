5f3993575dcaa2a37c9be31e074afa03b91ac914
Kepka Ludovic <shionn@gmail.com>
2026-08-12 13:49:05 +0200

    fsatab

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+echo "
+//192.168.10.157/isos/	/media/Isos    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/backups/	/media/Backups    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/medias/	/media/Medias    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/games/	/media/Games    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/dl/	/media/Dl    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/roms/	/media/Roms    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+
+192.168.10.157:/mnt/data/ludovic/	/media/Ludovic    nfs    defaults,auto    0    0
+192.168.10.157:/mnt/data/shared/	/media/Shared    nfs    defaults,auto    0    0
+192.168.10.157:/mnt/data-secure/ludovic/	/media/LudovicSecure    nfs    defaults,auto    0    0
+" | sudo tee -a /etc/fstab > /dev/null
+
 
 echo "[Unit]
 Description=Automount network drive
@@ -9,7 +24,7 @@ Restart=on-failure
 ExecStart=/usr/bin/mount -a
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/automount.service
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/automount.service > /dev/null
 
 sudo systemctl daemon-reload
 sudo systemctl status automount.service

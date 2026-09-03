281a520881d2559ce59d4ea753e9098228410a14
Kepka ludovic <shionn@bc250.lan>
2026-09-03 11:02:28 +0200

    ajout mount

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -7,12 +7,16 @@ echo "
 //192.168.10.157/games/	/media/Games    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
 //192.168.10.157/dl/	/media/Dl    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
 //192.168.10.157/roms/	/media/Roms    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/ludovic/	/media/Ludovic    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/shared/	/media/Shared    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
+//192.168.10.157/ludovic-secure/	/media/LudovicSecure    cifs    rw,user,suid,auto,uid=1000,gid=1000,credentials=/home/shionn/.smbcredentials    0    0
 
-192.168.10.157:/mnt/data/ludovic/	/media/Ludovic    nfs    defaults,auto    0    0
-192.168.10.157:/mnt/data/shared/	/media/Shared    nfs    defaults,auto    0    0
-192.168.10.157:/mnt/data-secure/ludovic/	/media/LudovicSecure    nfs    defaults,auto    0    0
+#192.168.10.157:/mnt/data/ludovic/	/media/Ludovic    nfs    defaults,auto    0    0
+#192.168.10.157:/mnt/data/shared/	/media/Shared    nfs    defaults,auto    0    0
+#192.168.10.157:/mnt/data-secure/ludovic/	/media/LudovicSecure    nfs    defaults,auto    0    0
 " | sudo tee -a /etc/fstab > /dev/null
 
+sudo mkdir /media/Isos /media/Backups /media/Medias /media/Games /media/Dl /media/Roms /media/Ludovic /media/Shared /media/LudovicSecure 
 
 echo "[Unit]
 Description=Automount network drive
